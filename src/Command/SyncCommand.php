3fbb9b248f2cd6103b16378244514312edc5d15a
Kacper <kacperrymkiewicz@gmail.com>
2024-05-15 20:13:27 +0000

    Add fetch method

--- src/Command/SyncCommand.php
+++ src/Command/SyncCommand.php
@@ -6,11 +6,19 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 // the name of the command is what users type after "php bin/console"
 #[AsCommand(name: 'sync:typo3')]
 class SyncCommand extends Command
 {
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        parent::__construct();
+        $this->doctrine = $doctrine;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         // ... put here the code to create the user
@@ -22,6 +30,7 @@ class SyncCommand extends Command
         // (it's equivalent to returning int(0))
         $io = new SymfonyStyle($input, $output);
         $io->note('Data synced successfully.');
+        $this->fetchTypo3Data();
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
         return Command::SUCCESS;
 
@@ -32,5 +41,20 @@ class SyncCommand extends Command
         // or return this to indicate incorrect command usage; e.g. invalid options
         // or missing arguments (it's equivalent to returning int(2))
         // return Command::INVALID
+    }
+
+    private function fetchTypo3Data()
+    {
+        $sql = 'SELECT * FROM tt_content';
+        
+        $connection = $this->doctrine->getConnection('typo3');
+        $statement = $connection->query($sql);
+
+
+        // Handle results, for example, display them
+        while (($row = $statement->fetchAssociative()) !== false) {
+            echo $row['bodytext'];
+        }
+
     }
 }
